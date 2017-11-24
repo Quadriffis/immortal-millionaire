25092772e72b11da5ba6d9884a39ae890de13316
Mentor Palokaj <mentor@palokaj.co>
2017-11-24 09:34:02 +0100

    Script newline betterr behavior

--- .sew.sh
+++ .sew.sh
@@ -1,19 +1,20 @@
 page='\n\n----\n\n'
+rm total.md
 cat 0* > total.md
-echo $page >> total.md
+echo -e $page >> total.md
 cat 1* >> total.md
-echo $page >> total.md
+echo -e $page >> total.md
 cat 2* >> total.md
-echo $page >> total.md
+echo -e $page >> total.md
 cat 3* >> total.md
-echo $page >> total.md
+echo -e $page >> total.md
 cat 4* >> total.md
-echo $page >> total.md
+echo -e $page >> total.md
 cat 5* >> total.md
-echo $page >> total.md
+echo -e $page >> total.md
 cat 6* >> total.md
-echo $page >> total.md
+echo -e $page >> total.md
 cat 7* >> total.md
-echo $page >> total.md
+echo -e $page >> total.md
 cat 8* >> total.md
 open total.md
