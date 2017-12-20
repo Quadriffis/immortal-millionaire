3cc229e5584146b96f633c9eda5ca1befaf549ac
Mentor Palokaj <mentor@palokaj.co>
2017-12-20 15:12:21 +0100

    Syntax fail

--- .sew.sh
+++ .sew.sh
@@ -1,30 +1,30 @@
 page='\n\n----\n\n'
 rm total.md
 
-cat 1.1* > total.md
+cat 1.1* >> total.md
 echo -e $page >> total.md
 
-cat 2.1* > total.md
+cat 2.1* >> total.md
 echo -e $page >> total.md
-cat 2.2* > total.md
+cat 2.2* >> total.md
 echo -e $page >> total.md
-cat 2.3* > total.md
+cat 2.3* >> total.md
 echo -e $page >> total.md
-cat 2.4* > total.md
+cat 2.4* >> total.md
 echo -e $page >> total.md
-cat 2.5* > total.md
-echo -e $page >> total.md
-
-cat 3.1* > total.md
-echo -e $page >> total.md
-cat 3.2* > total.md
-echo -e $page >> total.md
-cat 3.3* > total.md
+cat 2.5* >> total.md
 echo -e $page >> total.md
 
-cat 4.1* > total.md
+cat 3.1* >> total.md
 echo -e $page >> total.md
-cat 4.2* > total.md
+cat 3.2* >> total.md
+echo -e $page >> total.md
+cat 3.3* >> total.md
+echo -e $page >> total.md
+
+cat 4.1* >> total.md
+echo -e $page >> total.md
+cat 4.2* >> total.md
 echo -e $page >> total.md
 
 
